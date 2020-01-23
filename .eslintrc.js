926a7060837179afe5924365036c0b5b17a8d39e
ximing <ximing@meituan.com>
2020-01-23 20:22:21 +0800

    feat: 添加主框架

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'jest', 'react'],
+    extends: ['standard', 'prettier', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            { argsIgnorePattern: '^_', args: 'after-used' }
+        ],
+        '@typescript-eslint/no-empty-function': 0,
+        '@typescript-eslint/no-use-before-define': 0,
+        '@typescript-eslint/no-non-null-assertion': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/interface-name-prefix': 0,
+        '@typescript-eslint/member-delimiter-style': 0,
+        '@typescript-eslint/camelcase': 0,
+        '@typescript-eslint/no-namespace': 0,
+        'standard/no-callback-literal': 0,
+        'no-console': 0,
+        // 'no-console': ['error', { allow: ['warn', 'error'] }],
+        'prefer-spread': 0,
+        'prefer-rest-params': 0,
+        'no-prototype-builtins': 0,
+        'react/jsx-uses-vars': 'warn',
+        '@typescript-eslint/indent': 0,
+        camelcase: ['error', { allow: ['^internal_', '^unstable_'] }]
+    },
+    env: {
+        'jest/globals': true,
+        browser: true,
+        node: true,
+        es6: true
+    },
+    globals: {
+        testRule: 'readonly',
+        wx: 'readonly',
+        qq: 'readonly',
+        tt: 'readonly',
+        swan: 'readonly',
+        my: 'readonly',
+        getCurrentPages: 'readonly',
+        getApp: 'readonly',
+        requirePlugin: 'readonly',
+        jd: 'readonly',
+        Component: 'readonly',
+        App: 'readonly',
+        Page: 'readonly'
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
+};
